12ac66b8e18777a0ad85694ac37741f7549b682f
Melinda <melindmi@yahoo.com>
2017-10-11 15:04:53 +0100

    Update pnrretrieve.js

--- server/pnrretrieve.js
+++ server/pnrretrieve.js
@@ -1,39 +1,64 @@
 import axios from "axios"
 
-//import { srv } from "./config"
 import { isInactiveConversation } from "./utilepars"
 import session from "express-session"
 
 import SendLogin from "./login"
 
-function getPassengerName(data) {
+function getPNRDetails(data) {
+  var pnrDetails ={}
+
   if(data.Envelope){
     if(data.Envelope.Body){
       if(data.Envelope.Body.PNR_Reply){
         var r = data.Envelope.Body.PNR_Reply
+        //traveller info
         if(r.travellerInfo){
           if(r.travellerInfo.passengerData){
             var p = r.travellerInfo.passengerData
-            return p.travellerInformation.traveller.surname
+            pnrDetails.surname = p.travellerInformation.traveller.surname
+          }
+        }
+        
+        if(r.originDestinationDetails) {
+          if(r.originDestinationDetails.itineraryInfo) {
+            if(r.originDestinationDetails.itineraryInfo.travelProduct){
+              var tp = r.originDestinationDetails.itineraryInfo.travelProduct
+              //travel information
+              if (tp.product) {
+                pnrDetails.depDate = tp.product.depDate
+                pnrDetails.depTime = tp.product.depTime
+              }
+              
+              pnrDetails.depCity = tp.boardpointDetail ? tp.boardpointDetail.cityCode : ''
+              pnrDetails.arrCity = tp.offpointDetail ? tp.offpointDetail.cityCode : ''
+
+              //flight information
+              pnrDetails.airline = tp.companyDetail ? tp.companyDetail.identification : ''
+              pnrDetails.fligthNumber = tp.productDetails ? tp.productDetails.identification : ''
+
+              pnrDetails.flight = pnrDetails.airline + pnrDetails.fligthNumber
+            }
           }
         }
       }
     }
   }
 
-  return ""
-
+  return pnrDetails
 }
 
-function RetrievePNR(req) {
-  
-  var sessionDetails = req.session.sessionDetails
-  var auth = req.session.auth
+function RetrievePNR(session, pnr) {
+  var sessionDetails = session.sessionDetails
+  var auth = session.auth
 
   console.log("++++++++RetrievePNR session details++++++++++++++")
   console.log(sessionDetails)
   console.log("++++++++++++++++++++++++++++++++++")   
-  
+  console.log("++++++++++++++++++++++++++++++++++")   
+  console.log(pnr)
+  console.log("++++++++++++++++++++++++++++++++++")     
+
   var seq = parseInt(sessionDetails.seq, 10) + 1
 
   return axios
@@ -43,11 +68,12 @@ function RetrievePNR(req) {
       seqId: seq,
       token: sessionDetails.token,
       retType: "2",
-      ctrlNb: "RFYD8G"
+      ctrlNb: pnr
     }
   })
+
   .then( res => { 
-    if(res.data){ 
+    if (res.data) {  
       console.log("---------------- res data -----------------") 
       console.log(JSON.stringify(res.data))
     }
@@ -55,11 +81,14 @@ function RetrievePNR(req) {
       console.log("---------------- res -----------------") 
       console.log(JSON.stringify(res))
     }   
+
+    const pnrDetails = getPNRDetails(res.data)
+    session.pnrDetails = pnrDetails
+
+    console.log("PNRDetails is: " + JSON.stringify(pnrDetails))
     
-    var p = getPassengerName(res.data)
-    console.log("PassengerName is: " + p)
-    
-    return p })
+    return pnrDetails 
+  })
   
   .catch( error => { 
     console.log("---------------- error -----------------") 
@@ -68,13 +97,13 @@ function RetrievePNR(req) {
     if (isInactiveConversation(error.response.data)) {
       console.log("---------------- error +++++++++ call login again") 
       
-      return SendLogin(req, auth)
+      return SendLogin(session, auth)
       .then( rsp => RetrievePNR(req) )
       .catch( err => { throw new Error(JSON.stringify(err)) } )
     }
     else{
       console.log("---------------- error +++++++++ no inactive")
-      return error.response.data
+      throw new Error("PNR not found!") 
     }
   })
 }
