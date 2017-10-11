47939fde07e05d333ea2f750c43307ea41462c39
Melinda <melindmi@yahoo.com>
2017-10-11 15:24:27 +0100

    Update intents.js

--- server/intents.js
+++ server/intents.js
@@ -1,3 +1,3 @@
-export const DEFAULT_WELCOME = 'Default Welcome Intent'
+export const DEFAULT_WELCOME = 'Welcome - custom'
 export const RETRIEVE_PNR = 'Retrieve PNR'
 export const FLIGHT_AVAILABILITY = 'Flight availability'
