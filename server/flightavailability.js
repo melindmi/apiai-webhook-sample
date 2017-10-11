822f322b2090d5721cf2efb498e9bc9088322bf4
Melinda <melindmi@yahoo.com>
2017-10-11 17:27:12 +0100

    Update flightavailability.js

--- server/flightavailability.js
+++ server/flightavailability.js
@@ -5,6 +5,46 @@ import session from "express-session"
 
 import SendLogin from "./login"
 
+function getFlightsOptions(data){
+ 
+  var flightOpt = []
+
+  if(data.Envelope){  
+    if(data.Envelope.Body){
+      if(data.Envelope.Body.Air_MultiAvailabilityReply){
+        var m = data.Envelope.Body.Air_MultiAvailabilityReply
+        if(m.singleCityPairInfo){
+          var sg = m.singleCityPairInfo
+          if(sg.flightInfo){
+            for(var idx = 0; idx < sg.flightInfo.length; idx++) {
+                if(sg.flightInfo[idx].basicFlightInfo){
+                  var bf = sg.flightInfo[idx].basicFlightInfo
+                  var details = {}
+                  if(bf.flightDetails){
+                    var d = bf.flightDetails
+                    details.depDate = d.departureDate
+                    details.depTime = d.departureTime
+                    details.arrDate = d.arrivalDate
+                    details.arrTime = d.arrivalTime
+                  }
+
+                  details.depCity = bf.departureLocation ? bf.departureLocation.cityAirport : ''
+                  details.arrCity = bf.arrivalLocation ? bf.arrivalLocation.cityAirport : ''
+                  details.flightId = bf.flightIdentification ? bf.flightIdentification.number : ''
+                  details.airline = bf.marketingCompany ? bf.marketingCompany.identifier : ''
+                 
+                  flightOpt.push(details)
+                }
+            }
+          } 
+        }
+      }
+    }
+  }
+
+  return flightOpt
+}
+
 
 function FlightAvailability(session) {
   var sessionDetails = session.sessionDetails
@@ -41,8 +81,14 @@ function FlightAvailability(session) {
       console.log("---------------- res -----------------") 
       console.log(JSON.stringify(res))
     }   
-     
-    return res.data 
+    
+    console.log("Get Flights Options!!!!!!!!!!!!!!!!!" )
+    const flightsOpt = getFlightsOptions(res.data)
+    console.log("Flights Options are: " )
+    console.log(flightsOpt)
+    console.log("Flights Options are: " + JSON.stringify(flightsOpt))
+    
+    return flightsOpt 
   })
   
   .catch( error => { 
@@ -64,4 +110,5 @@ function FlightAvailability(session) {
 }
 
 export default FlightAvailability
+
 
